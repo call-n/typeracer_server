10673d2035d442b58502c78af3731224e68cb5f3
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-18 18:10:30 +0100

    Frontend endpoint

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const io = new socketio.Server(server, {
 	cors: {
 		origin: [
 			"http://localhost:3000",
+            "typeracer-dfwgcqwoz-call-n.vercel.app",
 		],
 		methods: ["GET", "POST"],
 	},
