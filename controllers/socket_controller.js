a8576bffb2931dece0a256f79a5f2ee37ef8f3c5
call-n <calle.nilsson1@ingka.ikea.com>
2023-01-22 13:05:43 +0100

    Formated files

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,4 +1,4 @@
-const shuffleList = require("../lib/functions")
+const shuffleList = require("../lib/functions");
 
 let io = null; // socket.io server instance
 
@@ -6,115 +6,116 @@ const playerRooms = {};
 
 const rooms = {};
 
-const disconnectHandler = function() {
-    const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
-    io.to("public").emit("online users", sockets.length);
+const disconnectHandler = function () {
+  const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+  io.to("public").emit("online users", sockets.length);
 
-    // the rooms player is currently in
-    const disconnectPlayerFrom = playerRooms[this.id];
-    if (!disconnectPlayerFrom) return;
-    disconnectPlayerFrom.forEach((roomId) => {
-        if (!rooms[roomId]) return;
-        const players = rooms[roomId].players;
-        rooms[roomId].players = players.filter((player) => {
-            if (player.id === this.id) {
-                io.in(roomId).emit("leave room", player.username);
-            }
-            return player.id !== this.id;
-        });
-
-        io.in(roomId).emit("room update", rooms[roomId].players);
-        if (rooms[roomId].players.length === 0) {
-            delete rooms[roomId];
-        }
-    });
-
-    // remove player
-    delete playerRooms[this.id];
-};
-
-const startGameHander = function(roomId) {
-    io.in(roomId).emit("words generated", rooms[roomId].toType);
-    io.in(roomId).emit("start game");
-    rooms[roomId].inGame = true;
-};
-
-const endGameHander = function(roomId, mode) {
-    const toType = shuffleList(mode).join(" ");
-    rooms[roomId] = {
-        players: rooms[roomId].players,
-        toType,
-        inGame: false,
-        winner: this.id,
-    };
-    console.log(this.id);
-    io.in(roomId).emit("winner", rooms[roomId].winner);
-    io.in(roomId).emit("end game", this.id);
-};
-
-
-const createRoomHandler = function(roomId, mode) {
-    if (io.sockets.adapter.rooms.get(roomId)) {
-        this.emit("room already exist");
-    } else {
-        const toType = shuffleList(mode).join(" ");
-        rooms[roomId] = {
-            players: [],
-            toType,
-            inGame: false,
-            winner: null,
-        };
-
-        this.emit("words generated", rooms[roomId].toType);
-        this.emit("create room success", roomId);
-    }
-};
-
-const updateRoomHandler = function(user) {
-    const { roomId } = user;
+  // the rooms player is currently in
+  const disconnectPlayerFrom = playerRooms[this.id];
+  if (!disconnectPlayerFrom) return;
+  disconnectPlayerFrom.forEach((roomId) => {
     if (!rooms[roomId]) return;
     const players = rooms[roomId].players;
-    rooms[roomId].players = players.map((player) => (player.id !== user.id ? player : user));
-    io.in(roomId).emit("room update", rooms[roomId].players);
-};
+    rooms[roomId].players = players.filter((player) => {
+      if (player.id === this.id) {
+        io.in(roomId).emit("leave room", player.username);
+      }
+      return player.id !== this.id;
+    });
 
-const joinRoomHander = function({ roomId, user }) {
-    this.emit("end game");
-    const room = rooms[roomId];
-    if (!room) {
-        this.emit("room invalid");
-        return;
-    } else if (rooms[roomId].inGame) {
-        this.emit("room in game");
-        return;
-    } else {
-        rooms[roomId].players = [...rooms[roomId].players, user];
-        playerRooms[this.id] = [roomId];
+    io.in(roomId).emit("room update", rooms[roomId].players);
+    if (rooms[roomId].players.length === 0) {
+      delete rooms[roomId];
     }
+  });
 
-    this.join(roomId);
-    this.emit("words generated", rooms[roomId].toType);
-    io.in(roomId).emit("room update", rooms[roomId].players);
-    this.to(roomId).emit("notify", `${user.username} is here.`);
-    console.log("join", rooms);
+  // remove player
+  delete playerRooms[this.id];
 };
 
-const leaveRoomHandler = function(user) {
-		const { roomId } = user;
-		const players = rooms[roomId];
-		if (!players) return;
-		rooms[roomId].players = players.players.filter((player) => {
-			if (player.id === user.id) {
-				this.to(roomId).emit("leave room", player.username);
-			}
-			return player.id !== user.id;
-		});
+const startGameHander = function (roomId) {
+  io.in(roomId).emit("words generated", rooms[roomId].toType);
+  io.in(roomId).emit("start game");
+  rooms[roomId].inGame = true;
+};
 
-		io.in(roomId).emit("room update", rooms[roomId].players);
-		if (rooms[roomId].players.length === 0) {
-			delete rooms[roomId];
-		}
-		console.log("leave ", rooms);
+const endGameHander = function (roomId, mode) {
+  const toType = shuffleList(mode).join(" ");
+  rooms[roomId] = {
+    players: rooms[roomId].players,
+    toType,
+    inGame: false,
+    winner: this.id,
+  };
+  console.log(this.id);
+  io.in(roomId).emit("winner", rooms[roomId].winner);
+  io.in(roomId).emit("end game", this.id);
+};
+
+const createRoomHandler = function (roomId, mode) {
+  if (io.sockets.adapter.rooms.get(roomId)) {
+    this.emit("room already exist");
+  } else {
+    const toType = shuffleList(mode).join(" ");
+    rooms[roomId] = {
+      players: [],
+      toType,
+      inGame: false,
+      winner: null,
+    };
+
+    this.emit("words generated", rooms[roomId].toType);
+    this.emit("create room success", roomId);
+  }
+};
+
+const updateRoomHandler = function (user) {
+  const { roomId } = user;
+  if (!rooms[roomId]) return;
+  const players = rooms[roomId].players;
+  rooms[roomId].players = players.map((player) =>
+    player.id !== user.id ? player : user
+  );
+  io.in(roomId).emit("room update", rooms[roomId].players);
+};
+
+const joinRoomHander = function ({ roomId, user }) {
+  this.emit("end game");
+  const room = rooms[roomId];
+  if (!room) {
+    this.emit("room invalid");
+    return;
+  } else if (rooms[roomId].inGame) {
+    this.emit("room in game");
+    return;
+  } else {
+    rooms[roomId].players = [...rooms[roomId].players, user];
+    playerRooms[this.id] = [roomId];
+  }
+
+  this.join(roomId);
+  this.emit("words generated", rooms[roomId].toType);
+  io.in(roomId).emit("room update", rooms[roomId].players);
+  this.to(roomId).emit("notify", `${user.username} is here.`);
+  console.log("join", rooms);
+};
+
+const leaveRoomHandler = function (user) {
+  const { roomId } = user;
+  const players = rooms[roomId];
+  if (!players) return;
+  rooms[roomId].players = players.players.filter((player) => {
+    if (player.id === user.id) {
+      this.to(roomId).emit("leave room", player.username);
+    }
+    return player.id !== user.id;
+  });
+
+  io.in(roomId).emit("room update", rooms[roomId].players);
+  if (rooms[roomId].players.length === 0) {
+    delete rooms[roomId];
+  }
+  console.log("leave ", rooms);
 };
 
 /**
@@ -122,24 +123,24 @@ const leaveRoomHandler = function(user) {
  *
  */
 module.exports = function (socket, _io) {
-	// save a reference to the socket.io server instance
-	io = _io;
+  // save a reference to the socket.io server instance
+  io = _io;
 
-    // console.log(io.sockets.adapter.rooms);
-	// console.log(sockets);
-	// console.log(socket.rooms);
-	console.log("connected, player:", socket.id);
+  // console.log(io.sockets.adapter.rooms);
+  // console.log(sockets);
+  // console.log(socket.rooms);
+  console.log("connected, player:", socket.id);
 
-	// handle user disconnect
-	socket.on('disconnect', disconnectHandler)
+  // handle user disconnect
+  socket.on("disconnect", disconnectHandler);
 
-	// game handlers
-	socket.on('start game', startGameHander)
-	socket.on('end game', endGameHander)
+  // game handlers
+  socket.on("start game", startGameHander);
+  socket.on("end game", endGameHander);
 
-	// room handlers
-	socket.on('create room', createRoomHandler)
-	socket.on('room update', updateRoomHandler)
-	socket.on('join room', joinRoomHander)
-	socket.on('leave room', leaveRoomHandler)
-}
+  // room handlers
+  socket.on("create room", createRoomHandler);
+  socket.on("room update", updateRoomHandler);
+  socket.on("join room", joinRoomHander);
+  socket.on("leave room", leaveRoomHandler);
+};
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const socketio = require('socket.io');
-const socket_controller = require('./controllers/socket_controller');
-const express = require('express');;
+const socketio = require("socket.io");
+const socket_controller = require("./controllers/socket_controller");
+const express = require("express");
 const http = require("http");
 const cors = require("cors");
 
 /**
  * Get port from environment.
  */
-const port = process.env.PORT || '8080';
+const port = process.env.PORT || "8080";
 const app = express();
 app.use(cors);
 
@@ -16,52 +16,49 @@ app.use(cors);
  */
 const server = http.createServer(app);
 const io = new socketio.Server(server, {
-	cors: {
-		origin: [
-			"http://localhost:3000",
-            "typeracer-dfwgcqwoz-call-n.vercel.app",
-		],
-		methods: ["GET", "POST"],
-	},
+  cors: {
+    origin: ["http://localhost:3000", "typeracer-dfwgcqwoz-call-n.vercel.app"],
+    methods: ["GET", "POST"],
+  },
 });
 
 /**
  * Handle incoming connections
  */
-io.on('connection', socket => {
-	socket_controller(socket, io);
+io.on("connection", (socket) => {
+  socket_controller(socket, io);
 });
 
 /**
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Event listener for HTTP server "error" event.
  */
 function onError(error) {
-	if (error.syscall !== 'listen') {
-		throw error;
-	}
+  if (error.syscall !== "listen") {
+    throw error;
+  }
 
-	// handle specific listen errors with friendly messages
-	switch (error.code) {
-		case 'EADDRINUSE':
-			console.error(`Port ${port} is already in use`);
-			process.exit(1);
-			break;
-		default:
-			throw error;
-	}
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EADDRINUSE":
+      console.error(`Port ${port} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 function onListening() {
-	const addr = server.address();
-	console.log(`Listening on port ${addr.port}`);
+  const addr = server.address();
+  console.log(`Listening on port ${addr.port}`);
 }
